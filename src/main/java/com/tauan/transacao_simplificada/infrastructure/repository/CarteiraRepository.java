4541bd7b878f6d98a17547027ece1262d2d4abf4
tauandosanjosnascimento <tauandosanjosnascimento@gmail.com>
2026-01-28 15:32:50 -0300

    feat: cria repositories

---
+++ src/main/java/com/tauan/transacao_simplificada/infrastructure/repository/CarteiraRepository.java
@@ -0,0 +1,7 @@
+package com.tauan.transacao_simplificada.infrastructure.repository;
+
+import com.tauan.transacao_simplificada.infrastructure.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+}
---
+++ src/main/java/com/tauan/transacao_simplificada/infrastructure/repository/UsuarioRepository.java
@@ -0,0 +1,2 @@
+package com.tauan.transacao_simplificada.infrastructure.repository;public interface UsuarioRepository {
+}
